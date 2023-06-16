e7d05f2a1638d7975400dbf68ed2155eebb982d6
Michael <mmills6060@gmail.com>
2023-06-16 01:04:23 -0400

    fwfwe

--- insertion_test.c
+++ insertion_test.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
     int dataset4[] = {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     int dataset5[] = {100, 201, 52, 3223, 24, 55, 623, 75, 8523, -9, 150};
     int dataset6[] = {-1, 1, 2, -3, 4, 5, -6, 7, 8, -9, 10};
-
+    int dataset7[] = {};
 
     int print = 1;
 
@@ -36,7 +36,8 @@ int main(int argc, char *argv[])
     printf("\n");
     insertionSortIntegers(dataset6, 11, print);
     printf("\n");
-
+    insertionSortIntegers(dataset7, 11, print);
+    printf("\n");
     
 
 
--- selection_test.c
+++ selection_test.c
@@ -20,6 +20,7 @@ int main(int argc, char *argv[])
     int dataset4[] = {2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
     int dataset5[] = {100, 201, 52, 3223, 24, 55, 623, 75, 8523, -9, 150};
     int dataset6[] = {-1, 1, 2, -3, 4, 5, -6, 7, 8, -9, 10};
+    int dataset7[] = {};
 
     int print = 1;
     unsigned int size = 11;
@@ -35,6 +36,8 @@ int main(int argc, char *argv[])
     selectionSortIntegers(dataset5, size, print);
     printf("\n");
     selectionSortIntegers(dataset6, size, print);
+    printf("\n");
+    selectionSortIntegers(dataset7, size, print);
     printf("\n");
     // Print out an array
    
